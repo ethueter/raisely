1168c35a9547e95096e574748dc18ab1493e974e
Tyler Hueter <tylerhueter08@gmail.com>
2020-03-03 14:57:44 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Applying to </h1> 
+        <img className="App-logo"src="https://raisely-images.imgix.net/raisely-website-2019/uploads/logo-p-pdark-small-png-d4ab65.png?fit=max&amp;w=1000&amp;auto=format&amp;q=62" alt="Raisely Website 2019"/>
       </header>
+      <h1>Testing</h1>
     </div>
   );
 }

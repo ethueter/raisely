d1829d90508eb778685eb92224fa8a9c52c6e0f4
Tyler Hueter <tylerhueter08@gmail.com>
2020-03-03 15:49:56 -0500

    adding navigation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Header } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Applying to </h1> 
         <img className="App-logo"src="https://raisely-images.imgix.net/raisely-website-2019/uploads/logo-p-pdark-small-png-d4ab65.png?fit=max&amp;w=1000&amp;auto=format&amp;q=62" alt="Raisely Website 2019"/>
       </header>
-      <h1>Testing</h1>
+      <div>
+        
+      </div>
+      <h1>Customer Success Engineer</h1>
     </div>
   );
 }
